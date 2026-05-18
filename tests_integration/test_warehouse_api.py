8094de649888c53fccfa37ac4b7b32bbe2f8237f
Demetrius Kattes <dlkattes@hotmail.com>
2026-05-18 19:24:56 -0400

    Edit warehouse api test response url

--- tests_integration/test_warehouse_api.py
+++ tests_integration/test_warehouse_api.py
@@ -1,7 +1,7 @@
 import httpx
 
 def test_warehouse_api_root():
-    response = httpx.get("http://localhost:8000/")
+    response = httpx.get("http://warehouse_api:8000/")
     assert response.status_code == 200
     data = response.json()
     assert data["status"] == "ok"

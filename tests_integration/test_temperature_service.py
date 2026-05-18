d6ca60ecedb0a0f5d79603d46721d63284b66e0d
Demetrius Kattes <dlkattes@hotmail.com>
2026-05-18 18:37:00 -0400

    Edit temp service test response url

--- tests_integration/test_temperature_service.py
+++ tests_integration/test_temperature_service.py
@@ -1,5 +1,7 @@
 import httpx
 
+BASE_URL = "http://temperature_service:8001"
+
 def test_temperature_freezer():
     response = httpx.get(f"{BASE_URL}/temperatures/freezer")
     assert response.status_code == 200
